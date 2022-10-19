0ff5575371d418e62896857e83c137fad0c0fd9c
Gabriel Bezerra <104681677+GabrielBezerraG@users.noreply.github.com>
2022-10-19 18:47:28 -0300

    Implemented desktop layout

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,22 +2,25 @@ import './Product.css'
 
 function Product() {
   return (
-    <main className="product">
-      <img src="/images/image-product-mobile.jpg" alt="Bottle of perfume" className="product__image mobile-only" />
-      <div className='product__info'>
-        <h3 className="product__info__category">PERFUME</h3>
-        <h1 className="product__info__title">Gabrielle Essence Eau De Parfum</h1>
-        <p className="product__info__description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
-        <div className='product__info__prices'>
-          <h2 className="product__info__prices__main">$149.99</h2>
-          <span className="product__info__prices__old">$169.99</span>
+    <div className='container'>
+      <main className="product">
+        <img src="/images/image-product-mobile.jpg" alt="Bottle of perfume" className="product__image--mobile" />
+        <img src="/images/image-product-desktop.jpg" alt="Bottle of perfume" className="product__image--desktop" />
+        <div className='product__info'>
+          <h3 className="product__info__category">PERFUME</h3>
+          <h1 className="product__info__title">Gabrielle Essence Eau De Parfum</h1>
+          <p className="product__info__description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+          <div className='product__info__prices'>
+            <h2 className="product__info__prices__main">$149.99</h2>
+            <span className="product__info__prices__old">$169.99</span>
+          </div>
+          <label className="product__info__button">
+            <i className="product__info__button__icon" />
+            <input className="product__info__button__input" type="button" value="Add to Cart" />
+          </label>
         </div>
-        <label className="product__info__button">
-          <i className="product__info__button__icon" />
-          <input className="product__info__button__input" type="button" value="Add to Cart" />
-        </label>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
 
